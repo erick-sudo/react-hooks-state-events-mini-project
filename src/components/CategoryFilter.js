a0071bb1df3cda2f2e8e68a6df1f2fee94d589e9
Erick Ochieng <erickochieng766@gmail.com>
2023-01-13 23:54:55 +0300

    REACT state

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,24 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories}) {
+
+  function toggleSelection(event) {
+    const buttons = Array.from(document.querySelectorAll(".buttons"))
+    buttons.forEach(button => {
+      button.classList.remove("selected");
+      });
+
+      event.target.classList.add("selected");
+    }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {
+        categories.map((category, index) => {
+          return (<button className="buttons" onClick={toggleSelection} key={category+index}>{category}</button>)
+        })
+      }
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function NewTaskForm() {
+function NewTaskForm({categories}) {
   return (
     <form className="new-task-form">
       <label>
@@ -10,7 +10,11 @@ function NewTaskForm() {
       <label>
         Category
         <select name="category">
-          {/* render <option> elements for each category here */}
+          {
+            categories.map(category => {
+              return (<option key={category} value={category}>{category}</option>)
+            })
+          }
         </select>
       </label>
       <input type="submit" value="Add task" />
